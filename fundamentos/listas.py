da272326f27a0dd1cfbfdc1bd35d4bc2c896f643
Luan Souza <alunoluansenai@gmail.com>
2025-08-21 13:21:44 -0300

    📚 Documentação: Métodos de Listas

--- fundamentos/listas.py
+++ fundamentos/listas.py
@@ -1,12 +1,11 @@
 # Listas
-
 frutas = ["Laranja", "Banana", "Maçâ", "Goaiaba", "Acerola"]
 vazia = []
 letras = list("Academia")
 numeros = list(range(20))
 carro = ["RAM", "RAMPAGE R/T", 2025, "Esportivo", "R$ 274.990", 20]
 
-
+# Matriz
 matriz = [
 
     [1, "a", 2],
@@ -41,12 +40,51 @@ print(lista[::-1])
 print(len(lista))
 print(lista[0:8:2])
 
+# Loop em lista de frutas
 for fruta in frutas:
     print(fruta)
 
+# Loop com index em lista de frutas
 for index, fruta in enumerate(frutas):
     print(f"index:{index} fruta:{fruta}")
 
+# Retorna os números pares
 pares = [numero for numero in numeros if numero % 2 == 0]
-
 print(pares)
+
+
+# Cópia de Lista
+listaCopia = lista.copy()
+print(lista)
+print(listaCopia)
+listaCopia.append("Y")
+print(lista)
+print(listaCopia)
+
+# Conta quantas vezes um item da lista se repete
+print(lista.count("a"))
+
+# Pega o index da primeira ocorrencia do item
+print(lista.index("A"))
+print(lista.index("c"))
+
+# Juntar listas
+lista.extend(listaCopia)
+print(lista)
+print(listaCopia)
+
+# A lista pode se comportar como pilha
+lista.pop() # Removeu o 'Y'
+lista.pop() # Removeu o 'a'
+lista.pop() # Removeu o 'i'
+lista.pop(0) # Removeu o item da index 0 'A'
+print(lista)
+
+# Conta quantas vezes um item da lista se repete
+print(lista.count("a"))
+# Remove a primeira ocorrencia do item
+lista.remove("a")
+print(lista.count("a"))
+print(lista)
+lista.reverse()
+print(lista)
